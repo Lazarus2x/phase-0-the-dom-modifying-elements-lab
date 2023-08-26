ead81341f68dd6144f3758c5a9a2f9e0d965b3fd
lazarus <lazarusr.garcia@gmail.com>
2023-08-26 13:22:02 -0500

    creating nodes

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Write your code here!
+const main = document.querySelector('#main').remove()
+
+let newHeader = document.createElement('h1')
+newHeader.setAttribute("id", "victory")
+newHeader.innerHTML = "Lazarus is the champion";
+document.body.append(newHeader)
+
+
+
+
+
+
+
+
